5f6d05ea5ec146703f009322333c1fdfc919f12f
MoinulAx <moinulkhan347@gmail.com>
2024-07-01 11:34:53 -0400

    server err fixed

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const cors = require("cors")
-
+const app = express()
 //Middleware
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req,res) => {
+    res.send('joe')
+})
+
+module.exports = app
